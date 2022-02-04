fe2ca54107125b9adc23ef8ec310ccf065dda6d3
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-02-04 14:27:52 +0900

    Initial commit

---
+++ src/main/java/com/passgym/PassgymbackApplication.java
@@ -0,0 +1,13 @@
+package com.passgym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassgymbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassgymbackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/passgym/PassgymbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.passgym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassgymbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
