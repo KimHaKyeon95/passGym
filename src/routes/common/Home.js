65cb55f0c850eacee2bde4a364758b4cf9fe0cf2
Kim Hak Hyeon 95 <81364044+KimHaKyeon95@users.noreply.github.com>
2022-02-07 18:59:21 +0900

    Revert "Revert "Merge branch 'master' into hak_owner""
    This reverts commit 37b546efdde7f3a06d5e17a425e606469d749b21.

---
+++ src/routes/common/Home.js
@@ -0,0 +1,13 @@
+import Header from "../../components/common/Header";
+import Footer from "../../components/common/Footer";
+function Home() {
+  return (
+    <div>
+      <Header />
+      <body>메인 바디</body>
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/routes/common/Login.js
@@ -0,0 +1,13 @@
+import Header from "../../components/common/Header";
+import Footer from "../../components/common/Footer";
+function Login() {
+  return (
+    <div>
+      <Header />
+      <body>로그인</body>
+      <Footer />
+    </div>
+  );
+}
+
+export default Login;
