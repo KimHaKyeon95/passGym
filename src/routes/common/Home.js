f9eb67131a02576b6c18b410c14fc8d66a9b68be
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-02-10 20:54:36 +0900

    feat: Update Home

--- src/routes/common/Home.js
+++ src/routes/common/Home.js
@@ -1,10 +1,58 @@
 import Header from "../../components/common/Header";
 import Footer from "../../components/common/Footer";
+import { useEffect, useState } from "react";
 function Home() {
+  const [loading, setLoading] = useState(true);
+  const [gyms, setGyms] = useState([]);
+  const getGyms = () => {
+    setGyms([
+      {
+        ownerNo: 1,
+        name: "체육관 이름",
+        addr: "목포시 연산동",
+        avgStar: 2.0,
+        distance: 1.5,
+      },
+      {
+        ownerNo: 2,
+        name: "체육관 이름",
+        addr: "목포시 연산동",
+        avgStar: 2.0,
+        distance: 1.5,
+      },
+      {
+        ownerNo: 1,
+        name: "체육관 이름",
+        addr: "목포시 연산동",
+        avgStar: 2.0,
+        distance: 1.5,
+      },
+    ]);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getGyms();
+  }, []);
   return (
     <div>
       <Header />
-      <section>메인 바디</section>
+      <section>
+        {loading ? (
+          <h1>Loading...</h1>
+        ) : (
+          <div>
+            {gyms.map((gym) => (
+              <div>
+                <div>ownerNo : {gym.ownerNo}</div>
+                <div>name : {gym.name}</div>
+                <div>addr : {gym.addr}</div>
+                <div>avgStar : {gym.avgStar}</div>
+                <div>distance : {gym.distance}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </section>
       <Footer />
     </div>
   );
