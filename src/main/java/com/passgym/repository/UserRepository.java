8c102ae471e6c27498f963c8773d65b5b07733eb
syoungcho <95994880+syoungcho@users.noreply.github.com>
2022-02-24 18:30:28 +0900

    feat: User Repository/Test

---
+++ src/main/java/com/passgym/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package com.passgym.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.passgym.user.entity.User;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+	User findById(String id);
+	
+	List <User> findByName(String name);
+	
+	List <User> findByNameAndPhoneNo(String name, String phoneNo);
+}
