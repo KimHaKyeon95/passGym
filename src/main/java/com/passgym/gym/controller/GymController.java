217870e81626cbbc18623c1a133e18d2561171d8
Kim Hak Hyeon 95 <81364044+KimHaKyeon95@users.noreply.github.com>
2022-02-27 15:01:57 +0900

    refactor: refactoring GymController.saveGym

--- src/main/java/com/passgym/gym/controller/GymController.java
+++ src/main/java/com/passgym/gym/controller/GymController.java
@@ -1,13 +1,10 @@
 package com.passgym.gym.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.passgym.exception.FindException;
 import com.passgym.gym.entity.Gym;
-import com.passgym.owner.entity.Owner;
 import com.passgym.pass.entity.Pass;
-import com.passgym.pass.entity.PassPK;
 import com.passgym.repository.GymRepository;
 import com.passgym.repository.OwnerRepository;
 import com.passgym.service.GymService;
@@ -17,9 +14,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("gym/*")
@@ -38,6 +36,9 @@ public class GymController {
 
 	@Autowired
 	OwnerRepository ownerRepository;
+
+	@Autowired
+	GymService gymService;
 	
 	@GetMapping("/{ownerNo}")
 	public Object gymDetail(@PathVariable(name="ownerNo") String ownerNo) {
@@ -98,83 +99,16 @@ public class GymController {
 	}
 
 	@PostMapping(value = "/gymregist", consumes = "multipart/form-data")
-	public void saveGym(@RequestParam("files") List<MultipartFile> files,
+	public String saveGym(@RequestParam("files") List<MultipartFile> files,
+						@RequestParam("detailFiles") List<MultipartFile> detailFiles,
 						@RequestParam("gymInfo") String gymInfo,
 						@RequestParam("passes") String passes) {
-
-		ObjectMapper mapper = new ObjectMapper();
-
 		try{
-			Map<String, String> submittedGym = mapper.readValue(gymInfo,
-					new TypeReference<Map<String, String>>(){});
-			logger.info(""+submittedGym.get("ownerNo"));
-
-			for(MultipartFile file : files) {
-				String imgName = (new Date().getTime()) + "" + (new Random().ints(1000, 9999).findAny().getAsInt());
-				String originFileName = file.getOriginalFilename();
-				String fileExtension = originFileName.substring(originFileName.lastIndexOf(".") + 1);
-				File imgDirectory = new File("C:/passGymImg/" + submittedGym.get("ownerNo") , imgName + "." + fileExtension);
-				if (!imgDirectory.exists()) {
-					imgDirectory.mkdirs();
-				}
-				file.transferTo(imgDirectory);
-			}
-
-			List<Map<String,String>> mapPasses = mapper.readValue(passes,
-					new TypeReference<List<Map<String, String>>>(){});
-			List<Pass> realPasses = new ArrayList<>();
-			SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd");
-			for (Map<String, String> pass : mapPasses) {
-				PassPK passPK = new PassPK();
-				passPK.setOwnerNo(submittedGym.get("ownerNo"));
-				passPK.setPassNo(Integer.parseInt(pass.get("passNo")));
-
-				Pass realPass = new Pass();
-				realPass.setPassPk(passPK);
-				realPass.setPassName(pass.get("passName"));
-				realPass.setPassPrice(Integer.parseInt(pass.get("passPrice")));
-				logger.info(pass.get("passDate"));
-				realPass.setPassDate(transFormat.parse(pass.get("passDate")));
-				realPass.setPassStatus(1);
-				realPass.setPassMonth(Integer.parseInt(pass.get("passMonth")));
-				realPass.setPauseCount(Integer.parseInt(pass.get("pauseCount")));
-				realPass.setPauseDate(Integer.parseInt(pass.get("pauseDate")));
-				realPass.setRemarks(pass.get("remarks"));
-
-				realPasses.add(realPass);
-			}
-
-			Gym realGym = new Gym();
-			realGym.setOwnerNo(submittedGym.get("ownerNo"));
-			realGym.setName(submittedGym.get("name"));
-			realGym.setPhoneNo(submittedGym.get("phoneNo"));
-			realGym.setZipcode(submittedGym.get("zipcode"));
-			realGym.setAddr(submittedGym.get("addr"));
-			realGym.setAddrDetail(submittedGym.get("addrDetail"));
-			realGym.setIntroduce(submittedGym.get("introduce"));
-			realGym.setNotice(submittedGym.get("notice"));
-			realGym.setOperatingTime(submittedGym.get("operatingProgram"));
-
-			String startHour = submittedGym.get("startHour");
-			String startMinute = submittedGym.get("startMinute");
-			String endHour = submittedGym.get("endHour");
-			String endMinute = submittedGym.get("endMinute");
-
-			String operatingTime = startHour + ":" + startMinute
-									+ " ~ " + endHour + ":" + endMinute;
-			realGym.setOperatingTime(operatingTime);
-
-			realGym.setExtraService(submittedGym.get("extraService"));
-			realGym.setEtc(submittedGym.get("etc"));
-
-			realGym.setPasses(realPasses);
-
-			Optional<Owner> owner = ownerRepository.findById(submittedGym.get("ownerNo"));
-			realGym.setOwner(owner.get());
-
-			gymRepository.save(realGym);
-		} catch(Exception e){
+			gymService.gymSetting(files, detailFiles, gymInfo, passes );
+		}catch(Exception e){
 			e.printStackTrace();
+			return "error";
 		}
+		return "ok";
 	}
 }
