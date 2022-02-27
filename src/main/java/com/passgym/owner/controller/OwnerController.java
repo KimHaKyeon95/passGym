adf268de852ecf2757ad783d9aa58194ccb8513a
Kim Hak Hyeon 95 <81364044+KimHaKyeon95@users.noreply.github.com>
2022-02-27 15:01:03 +0900

    refactor: refactoring OwnerController.login

--- src/main/java/com/passgym/owner/controller/OwnerController.java
+++ src/main/java/com/passgym/owner/controller/OwnerController.java
@@ -10,13 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
 @RestController
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping("/ownersignup")
+@RequestMapping("/owner")
 public class OwnerController {
 
     @Autowired
@@ -27,6 +26,22 @@ public class OwnerController {
 
     @Autowired
     ObjectMapper mapper;
+
+    @PostMapping("/login")
+    public String login(@RequestBody Map<String, String> loginInfo){
+        System.out.println(loginInfo.get("pwd"));
+        Optional<Owner> ownerOptional = ownerRepository.findOwnerById(loginInfo.get("id"));
+        if(!ownerOptional.isPresent()){
+            return "id fail";
+        }else{
+            System.out.println(ownerOptional.get().getPwd());
+            if(!loginInfo.get("pwd").equals(ownerOptional.get().getPwd())){
+                return "pwd fail";
+            }else{
+                return ownerOptional.get().getOwnerNo();
+            }
+        }
+    }
 
     @GetMapping("/iddupchk")
     public String idDupChk(@RequestParam String id){
@@ -41,17 +56,20 @@ public class OwnerController {
     @PostMapping("/signup")
     public ResponseEntity ownerSignup(@RequestBody Map<String, String> owner) throws IOException {
         Owner ownerSave = new Owner();
-        String signupJson = mapper.writeValueAsString(owner);
-        Map<String, String> transferedJson = new HashMap<>();
-        Map transferOwner = mapper.readValue(signupJson, transferedJson.getClass());
-        Optional<Owner> ownerOptional = ownerRepository.findById((String)transferOwner.get("ownerNo"));
-
+//        String signupJson = mapper.writeValueAsString(owner);
+//        Map<String, String> transferedJson = new HashMap<>();
+//        Map transferOwner = mapper.readValue(signupJson, transferedJson.getClass());
+//        Optional<Owner> ownerOptional = ownerRepository.findById((String)transferOwner.get("ownerNo"));
+        Optional<Owner> ownerOptional = ownerRepository.findById(owner.get("ownerNo"));
         if(ownerOptional.isPresent()){
             return new ResponseEntity<>("OwnerNo is exists", HttpStatus.OK);
         }else{
-            ownerSave.setId((String)transferOwner.get("id"));
-            ownerSave.setPwd((String)transferOwner.get("pwd"));
-            ownerSave.setOwnerNo((String)transferOwner.get("ownerNo"));
+//            ownerSave.setId((String)transferOwner.get("id"));
+//            ownerSave.setPwd((String)transferOwner.get("pwd"));
+//            ownerSave.setOwnerNo((String)transferOwner.get("ownerNo"));
+            ownerSave.setId(owner.get("id"));
+            ownerSave.setPwd(owner.get("pwd"));
+            ownerSave.setOwnerNo(owner.get("ownerNo"));
             ownerRepository.save(ownerSave);
             return new ResponseEntity<>(HttpStatus.OK);
         }
