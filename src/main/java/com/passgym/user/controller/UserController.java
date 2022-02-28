7807816e3d82112add881096731adf77c7e93078
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-02-28 12:01:00 +0900

    feat: new useredit

--- src/main/java/com/passgym/user/controller/UserController.java
+++ src/main/java/com/passgym/user/controller/UserController.java
@@ -2,7 +2,6 @@ package com.passgym.user.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -14,12 +13,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.passgym.exception.FindException;
+import com.passgym.exception.ModifyException;
+import com.passgym.exception.RemoveException;
 import com.passgym.gympass.entity.GymPass;
 import com.passgym.user.entity.User;
 import com.passgym.user.service.UserService;
@@ -45,11 +48,15 @@ public class UserController {
 //		}
 //		int userNo = sessionUser.getUserNo();
 		int userNo = 1;
+		Map<String, Object> returnMap = new HashMap<>();
+		
 		try {
 			User user = service.findById(userNo);
 			session.setAttribute("user", user);
 			String name = user.getName();
 			String id = user.getId();
+			String pwd = user.getPwd();
+			String phoneNo = user.getPhoneNo();
 			String addr = user.getAddr();
 			String addrDetail = user.getAddrDetail();
 			String zipcode = user.getZipcode();
@@ -58,6 +65,9 @@ public class UserController {
 			map.put("userNo", userNo);
 			map.put("name", name);
 			map.put("id", id);
+			map.put("pwd", pwd);
+			map.put("pwdChk", "");
+			map.put("phoneNo", phoneNo);
 			map.put("addr", addr);
 			map.put("addrDetail", addrDetail);
 			map.put("zipcode", zipcode);
@@ -65,18 +75,99 @@ public class UserController {
 			String result = objectMapper.writeValueAsString(map);
 			return result;
 		}catch(FindException e) {
-			Map<String, Object> returnMap = new HashMap<>();
 			returnMap.put("msg", e.getMessage());
 			returnMap.put("status", 0);
-			return returnMap;
 		} catch (JsonProcessingException e) {
 			e.printStackTrace();
-			Map<String, Object> returnMap = new HashMap<>();
 			returnMap.put("msg", e.getMessage());
 			returnMap.put("status", 0);
-			return returnMap;
 		}
+		return returnMap;
 	}
+	
+	@PutMapping("/")
+	public Object editUser(@RequestBody Map<String,Object> requestMap, HttpSession session) {
+		//세션에서 가져올것
+//		User sessionUser = (User)session.getAttribute("User");
+//		if(sessionUser == null) {
+//			logger.info("로그인 안됨");
+//		}
+//		int userNo = sessionUser.getUserNo();
+		int userNo = 1;
+		User user;
+		
+		//결과값
+		Map<String, Object> returnMap = new HashMap<>();
+		String msg = "";
+		int status = 0;
+		try {
+			user = service.findById(userNo);
+			//String id = user.getId();
+			String name = (String)requestMap.get("name");
+			String pwd = (String)requestMap.get("pwd");
+			String phoneNo = (String)requestMap.get("phoneNo");
+			String zipcode = (String)requestMap.get("zipcode");
+			String addr = (String)requestMap.get("addr");
+			String addrDetail = (String)requestMap.get("addrDetail");
+			
+			user.setName(name);
+			user.setPwd(pwd);
+			user.setPhoneNo(phoneNo);
+			user.setZipcode(zipcode);
+			user.setAddr(addr);
+			user.setAddrDetail(addrDetail);
+			
+			service.modifyUser(user);
+			session.setAttribute("user", user);
+			msg = "수정에 성공했습니다.";
+			status = 1;
+		} catch (FindException e) {
+			e.printStackTrace();
+			msg = e.getMessage();
+		} catch (ModifyException e) {
+			e.printStackTrace();
+			msg = e.getMessage();
+		}
+		returnMap.put("msg", msg);
+		returnMap.put("status", status);
+		return returnMap;
+	}
+	
+	@PutMapping("/withdrawal")
+	public Object withdrawalUser(HttpSession session) {
+		//세션에서 가져올것
+//		User sessionUser = (User)session.getAttribute("User");
+//		if(sessionUser == null) {
+//			logger.info("로그인 안됨");
+//		}
+//		int userNo = sessionUser.getUserNo();
+		int userNo = 1;
+		User user;
+		
+		//결과값
+		Map<String, Object> returnMap = new HashMap<>();
+		String msg = "";
+		int status = 0;
+		try {
+			user = service.findById(userNo);
+			user.setUserStatus(0);
+			
+			service.withdrawalUser(user);
+			session.removeAttribute("User");
+			msg = "회원탈퇴에 성공했습니다.";
+			status = 1;
+		} catch (FindException e) {
+			e.printStackTrace();
+			msg = e.getMessage();
+		} catch (RemoveException e) {
+			e.printStackTrace();
+			msg = e.getMessage();
+		}
+		returnMap.put("msg", msg);
+		returnMap.put("status", status);
+		return returnMap;
+	}
+	
 	
 	@GetMapping("/gympasses")
 	public Object getGympasses(HttpSession session) {
--- src/main/java/com/passgym/user/service/UserService.java
+++ src/main/java/com/passgym/user/service/UserService.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.passgym.exception.FindException;
+import com.passgym.exception.ModifyException;
+import com.passgym.exception.RemoveException;
 import com.passgym.repository.UserRepository;
 import com.passgym.user.entity.User;
 
@@ -13,6 +15,31 @@ public class UserService {
 	UserRepository userRepository;
 	
 	public User findById(int userNo) throws FindException{
-		return userRepository.findById(userNo).get();
+		User user;
+		try {
+			user = userRepository.findById(userNo).get();
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new FindException(e.getMessage());
+		}
+		return user;
+	}
+	
+	public void modifyUser(User user) throws ModifyException{
+		try {
+			userRepository.save(user);
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new ModifyException(e.getMessage());
+		}
+	}
+	
+	public void withdrawalUser(User user) throws RemoveException{
+		try {
+			userRepository.save(user);
+		}catch(Exception e) {
+			e.printStackTrace();
+			throw new RemoveException(e.getMessage());
+		}
 	}
 }
