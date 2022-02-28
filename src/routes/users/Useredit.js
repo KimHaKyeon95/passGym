ac17a9304e543acaad1923b64f0b01bc241f014c
Hyeongyu Lee <dlgusrb1913@naver.com>
2022-02-28 11:59:22 +0900

    feat: update Useredit

--- src/routes/users/Useredit.js
+++ src/routes/users/Useredit.js
@@ -8,24 +8,114 @@ import {
   Form,
 } from "react-bootstrap";
 import { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
 import axios from "axios";
+import PostcodeModal from "../../components/owner/PostcodeModal";
+import { useNavigate } from "react-router";
 
 function Useredit() {
   const [loading, setLoading] = useState(true);
   const [User, setUser] = useState({});
 
+  const [chkResults, setResults] = useState({
+    idDupChkResult: 0,
+    pwdChkResult: 0,
+  });
+  //비밀번호 형식 체크의 결과
+  const [chkPwdResult, setChkPwdResult] = useState({
+    result: false,
+    resultMsg: "",
+  });
+
+  const [postcodeModalShow, setPostcodeModalShow] = useState(false);
+
+  const navigate = useNavigate();
   const getUser = () => {
     const url = "http://localhost:9999/passgym/user/";
     axios
       .get(url)
-      .then(function (response) {
+      .then((response) => {
         setUser(response.data);
         setLoading(false);
       })
-      .catch(function (error) {
+      .catch((error) => {
         alert(error.response.status);
       });
+  };
+
+  const onChangeHandler = (event) => {
+    const { name, value } = event.target;
+    const nextValues = {
+      ...User,
+      [name]: value,
+    };
+    setUser(nextValues);
+  };
+
+  const changeAddress = (value) => {
+    console.log(value);
+    const nextValues = {
+      ...User,
+      addr: value.addr,
+      zipcode: value.zipcode,
+    };
+    setUser(nextValues);
+  };
+
+  //비밀번호 형식 체크
+  const checkPassword = (value) => {
+    //  8 ~ 10자 영문, 숫자 조합
+    const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
+    // 형식에 맞는 경우 true 리턴
+    setChkPwdResult({ result: regExp.test(value) });
+    return regExp.test(value);
+  };
+  //비밀번호 실시간체크
+  const onPwdChange = (event) => {
+    const { name, value } = event.target;
+    const nextValues = {
+      ...User,
+      [name]: value,
+    };
+    setUser(nextValues);
+
+    const nextResult = {
+      result: checkPassword(value),
+      resultMsg: "",
+    };
+    if (!nextResult.result) {
+      setChkPwdResult({
+        resultMsg: "비밀번호는 8~10자리 숫자와 영어 조합으로 입력해주세요.",
+      });
+      setResults({ ...chkResults, pwdChkResult: 0 });
+    }
+  };
+
+  const onSubmitHandler = (event) => {
+    const url = "http://localhost:9999/passgym/user/";
+    axios
+      .put(url, User)
+      .then((response) => {
+        console.log(response.data);
+        navigate("/mypage");
+      })
+      .catch((error) => {
+        alert(error.response.status);
+      });
+    event.preventDefault();
+  };
+
+  const withdrawal = (event) => {
+    const url = "http://localhost:9999/passgym/user/withdrawal";
+    axios
+      .put(url, {})
+      .then((response) => {
+        console.log(response.data);
+        //navigate("/mypage");
+      })
+      .catch((error) => {
+        alert(error.response.status);
+      });
+    event.preventDefault();
   };
 
   useEffect(() => {
@@ -78,15 +168,11 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextEmail"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     아이디
                   </Form.Label>
-                  <Col md="9" sm="10">
-                    <Form.Control
-                      plaintext
-                      readOnly
-                      defaultValue="email@example.com"
-                    />
+                  <Col lg="9" md="8" sm="10">
+                    <Form.Control plaintext readOnly defaultValue={User.id} />
                   </Col>
                 </Form.Group>
                 <Form.Group
@@ -94,14 +180,17 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextPassword"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     이름
                   </Form.Label>
-                  <Col md="9" sm="10">
+                  <Col lg="9" md="8" sm="10">
                     <Form.Control
                       type="text"
-                      placeholder="Text"
-                      value="이현규"
+                      placeholder="Name"
+                      name="name"
+                      value={User.name}
+                      onChange={onChangeHandler}
+                      required
                     />
                   </Col>
                 </Form.Group>
@@ -110,11 +199,19 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextPassword"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     비밀번호
                   </Form.Label>
-                  <Col md="9" sm="10">
-                    <Form.Control type="password" placeholder="Password" />
+                  <Col lg="9" md="8" sm="10">
+                    <Form.Control
+                      type="password"
+                      placeholder="Password"
+                      name="pwd"
+                      value={User.pwd}
+                      onChange={onPwdChange}
+                      required
+                    />
+                    <div className="msg">{chkPwdResult.resultMsg}</div>
                   </Col>
                 </Form.Group>
                 <Form.Group
@@ -122,14 +219,17 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextPassword"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     전화번호
                   </Form.Label>
-                  <Col md="9" sm="10">
+                  <Col lg="9" md="8" sm="10">
                     <Form.Control
                       type="text"
-                      placeholder="Text"
-                      value="01062329789"
+                      placeholder="Phone No"
+                      name="phoneNo"
+                      value={User.phoneNo}
+                      onChange={onChangeHandler}
+                      required
                     />
                   </Col>
                 </Form.Group>
@@ -138,18 +238,30 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextPassword"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     우편번호
                   </Form.Label>
-                  <Col md="5" sm="7">
+                  <Col lg="5" md="4" sm="7">
                     <Form.Control
                       type="text"
-                      placeholder="Text"
-                      value="12345"
+                      placeholder="zipcode"
+                      name="zipcode"
+                      value={User.zipcode}
+                      onChange={onChangeHandler}
+                      required
                     />
                   </Col>
+                  <PostcodeModal
+                    show={postcodeModalShow}
+                    onHide={() => {
+                      setPostcodeModalShow(false);
+                    }}
+                    setValues={changeAddress}
+                  />
                   <Col>
-                    <Button>주소찾기</Button>
+                    <Button onClick={() => setPostcodeModalShow(true)}>
+                      검색
+                    </Button>
                   </Col>
                 </Form.Group>
                 <Form.Group
@@ -157,14 +269,17 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextPassword"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     주소
                   </Form.Label>
-                  <Col md="9" sm="10">
+                  <Col lg="9" md="8" sm="10">
                     <Form.Control
                       type="text"
-                      placeholder="Text"
-                      value="수원시 팔달구"
+                      placeholder="address"
+                      name="addr"
+                      value={User.addr}
+                      onChange={onChangeHandler}
+                      required
                     />
                   </Col>
                 </Form.Group>
@@ -173,25 +288,28 @@ function Useredit() {
                   className="mb-3"
                   controlId="formPlaintextPassword"
                 >
-                  <Form.Label column md="3" sm="2">
+                  <Form.Label column lg="3" md="4" sm="2">
                     상세주소
                   </Form.Label>
-                  <Col md="9" sm="10">
+                  <Col lg="9" md="8" sm="10">
                     <Form.Control
                       type="text"
-                      placeholder="Text"
-                      value="포레스트 311호"
+                      placeholder="detail address"
+                      name="addrDetail"
+                      value={User.addrDetail}
+                      onChange={onChangeHandler}
+                      required
                     />
+                  </Col>
+                  <Col style={{ textAlign: "center", margin: "40px 0 20px 0" }}>
+                    <Button type="submit" onClick={onSubmitHandler}>
+                      저장하기
+                    </Button>
                   </Col>
                 </Form.Group>
                 <Row style={{ margin: "40px 0 20px 0" }}>
                   <Col style={{ textAlign: "center" }}>
-                    <Button>저장하기</Button>
-                  </Col>
-                </Row>
-                <Row style={{ margin: "40px 0 20px 0" }}>
-                  <Col style={{ textAlign: "center" }}>
-                    <Button>탈퇴하기</Button>
+                    <Button onClick={withdrawal}>탈퇴하기</Button>
                   </Col>
                 </Row>
               </Form>
