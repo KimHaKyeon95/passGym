09908997e7594c5355d71376a7fecf4ecbb647e9
Kim Hak Hyeon 95 <81364044+KimHaKyeon95@users.noreply.github.com>
2022-02-28 14:53:31 +0900

    feat: create GymSortDto, GymCompare

---
+++ src/main/java/com/passgym/gym/utility/GymCompare.java
@@ -0,0 +1,19 @@
+package com.passgym.gym.utility;
+
+import com.passgym.dto.GymSortDto;
+
+import java.util.Comparator;
+
+public class GymCompare implements Comparator<GymSortDto> {
+
+    @Override
+    public int compare(GymSortDto gymDto1, GymSortDto gymDto2) {
+        if(gymDto1.getDistance() > gymDto2.getDistance()){
+            return 1;
+        }else if(gymDto1.getDistance() < gymDto2.getDistance()){
+            return -1;
+        }else{
+            return 0;
+        }
+    }
+}
